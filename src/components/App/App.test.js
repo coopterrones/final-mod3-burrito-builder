c378b61beee0d28edee8eb15b859ae57476dd6c4
Cooper Terrones <cooper.terrones@outlook.com>
2021-01-19 11:51:33 -0700

    App apiCalls integration test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,36 @@
+import React from "react";
+import { screen, render, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import OrderForm from "../OrderForm/OrderForm";
+import userEvent from "@testing-library/user-event";
+import Orders from "../Orders/Orders";
+import App from "../App/App";
+import { apiCalls } from "../../apiCalls";
 
+jest.mock("../../apiCalls");
+
+describe("App", () => {
+  // beforeEach(() => {
+  //   apiCalls.getOrders.mockResolvedValue([
+  //     { id: 1, name: "Pat", ingredients: ["beans", "lettuce", "carnitas"] },
+  //     {
+  //       id: 2,
+  //       name: "Sam",
+  //       ingredients: ["steak", "pico de gallo", "lettuce", "carnitas"],
+  //     },
+  //   ]);
+  // });
+  it("Should get all data on load", async () => {
+    apiCalls.getOrders.mockResolvedValue([
+      { id: 1, name: "Pat", ingredients: ["beans", "lettuce", "carnitas"] },
+      {
+        id: 2,
+        name: "Sam",
+        ingredients: ["steak", "pico de gallo", "lettuce", "carnitas"],
+      },
+    ]);
+
+    render(<App />);
+    await waitFor(() => expect(apiCalls.getOrders).toHaveBeenCalledTimes(1));
+  });
+});
